d92432fc6daabf45202f28bd10dd362ad2a3d8d6
dwg787 <forthevision7@gmail.com>
2022-12-12 17:07:33 +0900

    vercel upload try2

--- src/pages/TodoDetail.jsx
+++ src/pages/TodoDetail.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import './Style.css';
+import './style.css';
 // import { viewTodo } from '../redux/modules/todoManager';
 
 function ViewTodoDetail() {
