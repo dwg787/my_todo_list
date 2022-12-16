653762de151beb88ab85965f74ddcf1f11376595
dwg787 <forthevision7@gmail.com>
2022-12-16 17:35:33 +0900

    Header, Footer 컴포넌트 Router부에서 수정

--- src/Router.js
+++ src/Router.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header/Header';
 import Layout from './components/Layout/Layout';
+import Footer from './components/Footer/Footer';
 import ViewTodoDetail from './pages/TodoDetail';
 import Home from './pages/Home';
 
 const Router = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Layout>
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/:id' element={<ViewTodoDetail />} />
         </Routes>
       </Layout>
+      <Footer />
     </BrowserRouter>
   );
 };
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,19 +1,19 @@
 // src/pages/home.js
 import React from 'react';
 import Layout from '../components/Layout/Layout';
-import Header from '../components/Header/Header';
+// import Header from '../components/Header/Header';
 import CreateNewTodo from '../components/InputSection/InputSection';
 import TodoList from '../components/TodoList/TodoList';
-import Footer from '../components/Footer/Footer';
+// import Footer from '../components/Footer/Footer';
 // import CustomButton from '../components/CustomButton';
 
 const Home = () => {
   return (
     <Layout>
-      <Header />
+      {/* <Header /> */}
       <CreateNewTodo />
       <TodoList />
-      <Footer />
+      {/* <Footer /> */}
     </Layout>
   );
 };
